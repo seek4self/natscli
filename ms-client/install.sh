87cf9f2c5f4fc4087e8e8e1a35814153bb90d5dd
mazhuang <2392368224@qq.com>
2022-09-20 18:32:59 +0800

    add install.sh

---
+++ ms-client/install.sh
@@ -0,0 +1,3 @@
+#/usr/bin/env bash
+rootDir=$(cd `dirname $0`;pwd)
+ln -sf $rootDir/ms-client /usr/local/bin/ms-client
--- pkg-natscli.sh
+++ pkg-natscli.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 pkgDir=$(basename $1)
 prefix="natstool"
